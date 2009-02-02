2f5ea050ee389c92ad1bb621346d02fbf948de2b
hro <>
2009-02-02 23:34:44 +0000

    Added SH support to pyelf

--- tools/pyelf/elf/abi/abi_module_list.py
+++ tools/pyelf/elf/abi/abi_module_list.py
@@ -1,1 +1,1 @@
-modules = ["amd64", "arm", "arm_linker_script", "common", "ia32", "ia64", "mips", "abi_module_list"]
+modules = ["amd64", "arm", "arm_linker_script", "common", "ia32", "ia64", "mips", "sh", "abi_module_list"]
