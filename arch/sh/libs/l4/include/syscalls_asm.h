20c5ecf0ebe4861f770d4fdbf6f2efe9ad7350c4
Hiromasa Shimada <happyshimahiro@gmail.com>
2009-03-02 18:34:25 +0900

    I added some defines but they are not enough.

--- arch/sh/libs/l4/include/syscalls_asm.h
+++ arch/sh/libs/l4/include/syscalls_asm.h
@@ -21,6 +21,35 @@
 #define SYSCALL_cap_control         0x0F
 #define SYSCALL_memory_copy         0x10
 
+/**
+ * Trap handler
+ */
+#define L4_TRAP_KPUTC               0xa0
+#define L4_TRAP_KGETC               0xa4
+#define L4_TRAP_KGETC_NB            0xa8
+#define L4_TRAP_KDEBUG              0xac
+#define L4_TRAP_GETUTCB             0xb0
+#define L4_TRAP_CKBR                0xe4
+/* L4_TRAP_KIP  was 0xb4 */
+#define L4_TRAP_KSET_THRD_NAME		0xb8
+#define L4_TRAP_KSET_OBJECT_NAME    0xb8
+#define L4_TRAP_GETCOUNTER          0xbc
+#define L4_TRAP_GETNUMTPS           0xc0
+#define L4_TRAP_GETTPNAME           0xc4
+#define L4_TRAP_TCCTRL              0xc8
+
+#define L4_TRAP_PMU_RESET           0xcc
+#define L4_TRAP_PMU_STOP            0xd0
+#define L4_TRAP_PMU_READ            0xd4
+#define L4_TRAP_PMU_CONFIG          0xd8
+
+#define L4_TRAP_GETTICK             0xe0
+#define L4_TRAP_WBTEST              0xec
+
+#define SYSCALL_TRAPANUM			0x31
+#define SYSCALL_TRAPNUM_IPC			0x30
+
+
 #define SYSNUM(name)                SYSCALL_ ## name
 #define SYSCALL_REG                 r8
 #define SYSCALL_NUM                 0x31
